c5292569158a24b8ae1942cac89c88226aa731fa
manewton <wiscstatman@gmail.com>
2025-09-14 22:20:55 -0500

    more 5 extra

--- docxRkey/circle2.R
+++ docxRkey/circle2.R
@@ -23,8 +23,6 @@ A[3,1] <- 1
 g.star5 <- graph.adjacency(A)
 
 
-
-
 A <- matrix(0, 5, 5 )
 A[1,2] <- 1; A[2,1] <- 1
 A[1,3] <- 1; A[3,1] <- 1
@@ -48,10 +46,17 @@ A[2,1] <- 1; A[2,4] <- 1
 A[3,2] <- 1; A[3,5] <- 1
 A[4,1] <- 1; A[4,3] <- 1
 A[5,4] <- 1; A[5,2] <- 1
-
 g.feastp1 <- graph.adjacency(A)
 
 
+## another feast
+A <- matrix(0, 5, 5 )
+A[1,4] <- 1
+A[2,5] <- 1
+A[3,1] <- 1
+A[4,2] <- 1
+A[5,3] <- 1
+g.feastpIII <- graph.adjacency(A)
 
 
 
@@ -155,5 +160,28 @@ plot( g.feastp1,  layout=ll, vertex.size=30, edge.arrow.size=.8, edge.arrow.widt
 
 dev.off()
 
+
+
+pdf( file="feast5pIII.pdf", height=6, width=6 )
+labs <- c("A", "B", "C", "D", "E" )
+
+
+th <- c( pi/2, pi/2 - 2*pi/5 , pi/2 - 4*pi/5, pi/2 - 6*pi/5, pi/2 - 8*pi/5 )
+yy <- sin(th)
+xx <- cos(th)
+ll <- cbind( xx, yy )
+
+
+plot( g.feastpIII,  layout=ll, vertex.size=30, edge.arrow.size=.8, edge.arrow.width=.8,
+                 edge.lty=1, edge.color="black", vertex.color="grey",
+        edge.width=2, margin=c(0,.4,0,.4), vertex.label=labs, vertex.label.cex=2 )
+
+symbols( 0, 0, circles=1, add=TRUE , lwd=1/2, inches=FALSE , col="grey")
+
+plot( g.feastpIII,  layout=ll, vertex.size=30, edge.arrow.size=.8, edge.arrow.width=.8,
+                 edge.lty=1, edge.color="black", vertex.color="grey",
+        edge.width=2, margin=c(0,.4,0,.4), vertex.label=labs, add=TRUE , vertex.label.cex=2)
+
+dev.off()
 
 
