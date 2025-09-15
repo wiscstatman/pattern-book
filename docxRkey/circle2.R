c0483456c54a92fa15fe9f37214de9823d4f679e
manewton <wiscstatman@gmail.com>
2025-09-15 10:02:16 -0500

    more stars

--- docxRkey/circle2.R
+++ docxRkey/circle2.R
@@ -38,8 +38,6 @@ A[4,5] <- 1; A[5,4] <- 1
 
 g.feast <- graph.adjacency(A)
 
-
-
 A <- matrix(0, 5, 5 )
 A[1,3] <- 1; A[1,5] <- 1
 A[2,1] <- 1; A[2,4] <- 1
@@ -57,6 +55,16 @@ A[3,1] <- 1
 A[4,2] <- 1
 A[5,3] <- 1
 g.feastpIII <- graph.adjacency(A)
+
+
+## triply star I
+A <- matrix(0, 5, 5 )
+A[1,3] <- 1; A[1,4] <- 1
+A[2,4] <- 1; A[2,5] <- 1
+A[3,1] <- 1; A[3,5] <- 1
+A[4,2] <- 1; A[4,1] <- 1
+A[5,3] <- 1; A[4,2] <- 1
+g.triplyI  <- graph.adjacency(A)
 
 
 
@@ -183,5 +191,29 @@ plot( g.feastpIII,  layout=ll, vertex.size=30, edge.arrow.size=.8, edge.arrow.wi
         edge.width=2, margin=c(0,.4,0,.4), vertex.label=labs, add=TRUE , vertex.label.cex=2)
 
 dev.off()
+
+
+
+pdf( file="triplyI.pdf", height=6, width=6 )
+labs <- c("A", "B", "C", "D", "E" )
+
+
+th <- c( pi/2, pi/2 - 2*pi/5 , pi/2 - 4*pi/5, pi/2 - 6*pi/5, pi/2 - 8*pi/5 )
+yy <- sin(th)
+xx <- cos(th)
+ll <- cbind( xx, yy )
+
+
+plot( g.triplyI,  layout=ll, vertex.size=30, edge.arrow.size=.8, edge.arrow.width=.8,
+                 edge.lty=1, edge.color="black", vertex.color="grey",
+        edge.width=2, margin=c(0,.4,0,.4), vertex.label=labs, vertex.label.cex=2 )
+
+symbols( 0, 0, circles=1, add=TRUE , lwd=1/2, inches=FALSE , col="grey")
+
+plot( g.triplyI,  layout=ll, vertex.size=30, edge.arrow.size=.8, edge.arrow.width=.8,
+                 edge.lty=1, edge.color="black", vertex.color="grey",
+        edge.width=2, margin=c(0,.4,0,.4), vertex.label=labs, add=TRUE , vertex.label.cex=2)
+
+dev.off() 
 
 
