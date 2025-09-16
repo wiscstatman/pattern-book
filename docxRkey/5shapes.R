e4167d4e9e53fb512affc5bafbf46620daf55a78
manewton <wiscstatman@gmail.com>
2025-09-16 00:04:53 -0500

    2v3 extra

--- docxRkey/5shapes.R
+++ docxRkey/5shapes.R
@@ -152,3 +152,38 @@ library(igraph)
 
 
 
+## 5 for chocolate +
+
+A <- matrix(0, 5, 5 )
+A[1,3] <- 1; A[3,1] <- 1
+A[1,4] <- 1; A[4,1] <- 1
+A[2,4] <- 1; A[4,2] <- 1
+A[2,5] <- 1; A[5,2] <- 1
+
+# get the shapes right for A B vs C D E
+ll.a <- rbind( c(-1/2,2), c(1/2,2), c( -1,0 ), c(0,0), c(1,0) )
+
+g <- graph.adjacency(A)
+
+pdf( file="5h.pdf" )
+labs <- c("A", "B", "C", "D", "E" )
+
+th <- c( pi/2, pi/2 - 2*pi/5 , pi/2 - 4*pi/5, pi/2 - 6*pi/5, pi/2 - 8*pi/5 )
+yy <- sin(th)
+xx <- cos(th)
+ll <- cbind( xx, yy )
+
+
+plot( g,  layout=ll.a, vertex.size=30, edge.arrow.size=.8, edge.arrow.width=.8,
+                 edge.lty=1, edge.color="black", vertex.color="grey",
+        edge.width=2, margin=c(0,.4,0,.4), vertex.label=labs, vertex.label.cex=2 )
+
+plot( g,  layout=ll.a, vertex.size=30, edge.arrow.size=.8, edge.arrow.width=.8,
+                 edge.lty=1, edge.color="black", vertex.color="grey",
+        edge.width=2, margin=c(0,.4,0,.4), vertex.label=labs, add=TRUE , vertex.label.cex=2)
+
+dev.off()
+
+
+
+
